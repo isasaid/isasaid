786f5cf89b2b057d4ddac0278216ed25f89899e9
isasaid <isa.muhammad.said@gmail.com>
2015-10-07 02:54:59 -0700

    portfolio
    portfolio

---
+++ assets/nudie.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function(){
+
+	jQuery('#header .search-toggle button').click(function(){
+		jQuery('#header .search-form .form-group').animate({width: 'toggle'});
+		jQuery('#header .search-form .form-group input.search-text').focus();
+	});
+
+	jQuery(".entry-content").fitVids();
+
+	var resized;
+
+	jQuery(window).resize(function() {
+
+		clearTimeout(resized);
+
+		resized = setTimeout(function() {
+
+			if ( Modernizr.mq('(max-width:767px)' ) ) {
+
+				jQuery('#header .search-form .form-group').removeAttr('style'); 
+
+			}
+
+		}, 100);
+
+	});
+
+	jQuery('form.search-form').submit(function(e){
+
+		if( jQuery.trim( jQuery(this).find('input.search-text').val() ) == "" ){
+			return false;
+		}
+
+	});
+
+});
+
